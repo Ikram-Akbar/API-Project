ab632c6fbccc645ca1c3a013b05f2f2721ad262b
Ikram-Git-Coder <ikramakbar71@gmail.com>
2022-11-16 23:40:57 +0600

    complete integration of API

---
+++ Script/app.js
@@ -0,0 +1,9 @@
+const loadQuote = () => {
+  fetch("https://api.kanye.rest/")
+    .then((response) => response.json())
+    .then((data) => displayQuote(data));
+};
+const displayQuote = (data) => {
+  const quoteElement = document.getElementById("quote");
+  quoteElement.innerText = data.quote;
+};
